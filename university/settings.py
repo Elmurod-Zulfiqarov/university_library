51352c477d32639be202d7daa2c580bf9152ec54
Elmurod <77602143+Elmurod-Zulfiqarov@users.noreply.github.com>
2022-04-03 15:36:58 +0500

    secret-key

--- university/settings.py
+++ university/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xrae)e4p)&3qqufpt*ch*zfp+nq^n$quc8d2xs7kh34^p%w6e!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
